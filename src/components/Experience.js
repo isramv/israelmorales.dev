d40ead89a9b1914cdbb0586795c004b2d56efd14
Israel Morales <isramv@gmail.com>
2020-10-04 10:47:44 -0500

    experience education paragraph

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Experience(props) {
     return (
-        <div className='bg-gray-200 py-4'>
+        <div className='bg-gray-200 py-4 px-4 md:px-0'>
             <div className="container mx-auto">
                 <h3 className='text-3xl'>{props.data.title}:</h3>
                 {props.data.collection.map(item => {
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -4,8 +4,8 @@ export default function Paragraph(props) {
   return (
     <div className='py-4 bg-gray-200'>
       <div className="container mx-auto mb-4 px-4 md:px-0">
-        <h3 className="mt-2">{props.data.title}:</h3>
-        <div className="text-lg text-gray-700" dangerouslySetInnerHTML={{__html: props.data.text}}></div>
+        <h3>{props.data.title}:</h3>
+        <p className="text-lg text-gray-700" dangerouslySetInnerHTML={{__html: props.data.text}}></p>
       </div>
     </div>
   )
